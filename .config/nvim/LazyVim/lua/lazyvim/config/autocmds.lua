e6529dad82b11e44a18d7638f3274489796e923b
Iqrar <iqrar.h8197@gmail.com>
2026-08-30 08:12:02 +0500

    add files/dirs

--- .config/nvim/LazyVim/lua/lazyvim/config/autocmds.lua
+++ .config/nvim/LazyVim/lua/lazyvim/config/autocmds.lua
@@ -444,3 +444,17 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end)
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("man_unlisted"),
+  pattern = { "man", "help" },
+  callback = function(event)
+    local buf = vim.api.nvim_get_current_buf()
+    vim.bo[buf].buflisted = true
+    vim.bo[buf].buftype = "" -- remove nofile so it shows in buffers picker
+    vim.bo[buf].bufhidden = "hide" -- ← keep buffer alive so C-^ / e # can reach it
+
+    vim.keymap.del("n", "j", { buffer = true })
+    vim.keymap.del("n", "k", { buffer = true })
+  end,
+})
--- .config/nvim/after/ftplugin/help.lua
+++
@@ -1,5 +0,0 @@
-local buf = vim.api.nvim_get_current_buf()
-vim.bo[buf].buflisted = true
-vim.bo[buf].buftype = "" -- remove nofile so it shows in buffers picker
-vim.bo[buf].bufhidden = "hide" -- ← keep buffer alive so C-^ / e # can reach it
-vim.opt.number = true
